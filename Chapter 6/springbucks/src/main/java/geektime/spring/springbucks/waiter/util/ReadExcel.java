28cbef5b31e9fd5c37dd65fd75c84f9ddfd170e7
Yang <qi.b.yang@accenture.com>
2021-03-05 19:03:53 +0800

    try use stack maintain parent nodes

--- Chapter 6/springbucks/src/main/java/geektime/spring/springbucks/waiter/util/ReadExcel.java
+++ Chapter 6/springbucks/src/main/java/geektime/spring/springbucks/waiter/util/ReadExcel.java
@@ -11,6 +11,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class ReadExcel {
 
@@ -18,98 +19,114 @@ public class ReadExcel {
 
     // 维护一个 对象节点 hashmap
     HashMap<String, Node> complexHashMap = new HashMap<>();
+    // 用一个Stack维护 父节点
+    Stack<Node> complexStack = new Stack<>();
 
     private void readExcel(){
         try {
             //String encoding = "GBK";
             File excel = new File(excelPath);
             //判断文件是否存在
-            if (excel.isFile() && excel.exists()) {
-                //.是特殊字符，需要转义！！！！！
-                String[] split = excel.getName().split("\\.");
-                Workbook wb;
-                //根据文件后缀（xls/xlsx）进行判断
-                if ( "xls".equals(split[1])){
-                    //文件流对象
-                    FileInputStream fis = new FileInputStream(excel);
-                    wb = new HSSFWorkbook(fis);
-                }else if ("xlsx".equals(split[1])){
-                    wb = new XSSFWorkbook(excel);
-                }else {
-                    System.out.println("文件类型错误!");
-                    return;
-                }
+                if (excel.isFile() && excel.exists()) {
+                    //.是特殊字符，需要转义！！！！！
+                    String[] split = excel.getName().split("\\.");
+                    Workbook wb;
+                    //根据文件后缀（xls/xlsx）进行判断
+                    if ( "xls".equals(split[1])){
+                        //文件流对象
+                        FileInputStream fis = new FileInputStream(excel);
+                        wb = new HSSFWorkbook(fis);
+                    }else if ("xlsx".equals(split[1])){
+                        wb = new XSSFWorkbook(excel);
+                    }else {
+                        System.out.println("文件类型错误!");
+                        return;
+                    }
 
-                //开始解析  //读取sheet 0
-               //  Sheet sheet = wb.getSheetAt(0);
-                Sheet sheet = wb.getSheet("(HEC) IN OUT");
-                //第一行是列名，所以不读 // 测试 从 5 开始读是 pcXXXWSRequest
-                int firstRowIndex = 5;
-                int lastRowIndex = sheet.getLastRowNum();
-                System.out.println("firstRowIndex: "+firstRowIndex);
-                System.out.println("lastRowIndex: "+lastRowIndex);
+                    //开始解析  //读取sheet 0
+                   //  Sheet sheet = wb.getSheetAt(0);
+                    Sheet sheet = wb.getSheet("(HEC) IN OUT");
+                    //第一行是列名，所以不读 // 测试 从 5 开始读是 pcXXXWSRequest
+                    int firstRowIndex = 5;
+                    int lastRowIndex = sheet.getLastRowNum();
+                    System.out.println("firstRowIndex: "+firstRowIndex);
+                    System.out.println("lastRowIndex: "+lastRowIndex);
 
-                // 记录前一条记录的 nodeName 单元格列号
-                int preColumnIndex = 0;
-                Node preNode = Node.builder().build();
-                // 记录当前所在结构体的 父节点 nodeName
-//                String parentNodeName = "rootNoParent";
+                    // 记录前一条记录的 nodeName 单元格列号
+                    int preColumnIndex = 0;
+                    Node preNode = Node.builder().build();
+                    // 记录当前所在结构体的 父节点 nodeName
+    //                String parentNodeName = "rootNoParent";
 
-                //遍历行
-                for(int rIndex = firstRowIndex; rIndex <= lastRowIndex; rIndex++) {
-                    System.out.println("rIndex: " + rIndex);
-                    Row row = sheet.getRow(rIndex);
-                    if (row != null) {
-                        // 遍历前部分列 确认 colIndex 经测试 从 1 B 开始 11 K 结束
-                        int firstCellIndex = 1;
-                        int endCellIndex = 11;
-                        for(int cIndex = firstCellIndex;cIndex<endCellIndex;cIndex++){
-                            Cell tempCell = row.getCell(cIndex);
-                            // 过滤出只有 nodeName 的 Cell
-                            if (tempCell!=null && !tempCell.getStringCellValue().isEmpty()) {
-                                String tempKey = firstRowIndex + "_" + cIndex + "_" + tempCell.getStringCellValue();
-                                System.out.println("firstRowIndex+\"_\"+cIndex+\"_\"+tempCell.toString() = " + tempKey);
+                    //遍历行
+                    for(int rIndex = firstRowIndex; rIndex <= lastRowIndex; rIndex++) {
+                        System.out.println("rIndex: " + rIndex);
+                        Row row = sheet.getRow(rIndex);
+                        if (row != null) {
+                            // 遍历前部分列 确认 colIndex 经测试 从 1 B 开始 11 K 结束
+                            int firstCellIndex = 1;
+                            int endCellIndex = 11;
+                            for(int cIndex = firstCellIndex;cIndex<endCellIndex;cIndex++){
+                                Cell tempCell = row.getCell(cIndex);
+                                // 过滤出只有 nodeName 的 Cell
+                                if (tempCell!=null && !tempCell.getStringCellValue().isEmpty()) {
+                                    String tempKey = firstRowIndex + "_" + cIndex + "_" + tempCell.getStringCellValue();
+                                    System.out.println("firstRowIndex+\"_\"+cIndex+\"_\"+tempCell.toString() = " + tempKey);
 
-                                String nodeName = tempCell.getStringCellValue();
-                                String type = row.getCell(13).getStringCellValue();
-                                String min = row.getCell(14).toString();
-                                String max = row.getCell(15).toString();
+                                    String nodeName = tempCell.getStringCellValue();
+                                    String type = row.getCell(13).getStringCellValue();
+                                    String min = row.getCell(14).toString();
+                                    String max = row.getCell(15).toString();
 
-                                Node tempNode = Node.builder().name(nodeName)
-//                                        .parentName(parentNodeName)
-                                        .type(type)
-                                        .minOccurs(min)
-                                        .maxOccurs(max).build();
-
-                                if("complex".equals(type)){
-                                    complexHashMap.put(nodeName,tempNode);
-                                }
-
-                                System.out.println("前一条记录列号：" + preColumnIndex +" tempNode = " + tempNode);
-
-                                // 当前节点是前序节点的子节点
-                                if(cIndex > preColumnIndex){
-
-                                    // 设置父节点
+                                    Node tempNode = Node.builder().name(nodeName)
+    //                                        .parentName(parentNodeName)
+                                            .type(type)
+                                            .minOccurs(min)
+                                            .maxOccurs(max).build();
                                     // 首次执行为空
+                                    if(complexHashMap.isEmpty() && "complex".equals(type)){
+                                        complexHashMap.put(nodeName,tempNode);
+                                        complexStack.push(tempNode);
+                                    }
 
-                                } else if (preColumnIndex > cIndex){
-                                    // 当前节点是一个新的 complex 结构体
-                                    // 设置父节点，前序节点的父节点 的父节点
+                                    System.out.println("前一条记录列号：" + preColumnIndex +" tempNode = " + tempNode);
 
-                                } else {
-                                    // 当前节点和前序节点平级
-                                    // 设置父节点，前序节点的父节点
+                                    Node parent = complexStack.peek();
 
+                                    // 当前节点是前序节点的子节点
+                                    if(cIndex > preColumnIndex){
+
+                                        if("complex".equals(type)){
+                                            parent.addKids(tempNode);
+                                        }else{
+                                            parent.addFields(tempNode);
+                                        }
+                                        complexHashMap.put(parent.getName(),parent);
+                                    } else if (preColumnIndex > cIndex){
+                                        // 当前节点是一个新的 complex 结构体
+                                        complexStack.pop();
+                                        complexStack.push(tempNode);
+                                        parent = complexStack.peek();
+                                        complexHashMap.put(parent.getName(),tempNode);
+                                    } else {
+                                        // 当前节点和前序节点平级
+                                        // 设置父节点，前序节点的父节点
+                                        if("complex".equals(type)){
+                                            parent.addKids(tempNode);
+                                            complexStack.push(tempNode);
+                                        }else{
+                                            parent.addFields(tempNode);
+                                        }
+                                        complexHashMap.put(parent.getName(),parent);
+                                    }
+
+                                    preColumnIndex = cIndex;
+                                    preNode = tempNode;
                                 }
-
-                                preColumnIndex = cIndex;
-                                preNode = tempNode;
                             }
                         }
                     }
-                }
-            } else {
+                } else {
                 System.out.println("找不到指定的文件");
             }
         } catch (Exception e) {
